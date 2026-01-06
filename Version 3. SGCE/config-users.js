cabe96625281184f6163809156f60f354d3524a2
XelleFer <luis.mendoza@xellescientific.com>
2026-01-06 13:31:26 -0600

    Actualizando archivos de Version 3. SGCE

--- Version 3. SGCE/config-users.js
+++ Version 3. SGCE/config-users.js
@@ -3,6 +3,6 @@ const AUTHORIZED_USERS = [
     { user: "Xelle_Admin", pass: "Xelle", role: "admin", name: "Administrador" },
     { user: "Xelle_Lab", pass: "Xelle_Calidad", role: "calidad", name: "Lab. Calidad" },
     { user: "Xelle_Banco", pass: "Xelle_Cultivo", role: "banco", name: "Banco Células" },
-    { user: "Xelle_Cultivo", pass: "Xelle_Cultivo", role: "banco", name: "Fanny" }
+    { user: "Xelle_Cultivo", pass: "Xelle_Cultivo", role: "banco", name: "Fanny" },
     { user: "Xelle_Nat", pass: "Xelle", role: "admin", name: "Administrador" }
 ];
